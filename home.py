12d52b486678f3788fa1d85b4c39bade8b8ef491
pierre-louisN <nohetpl@gmail.com>
2021-01-12 14:16:04 +0100

    changements market et home

--- home.py
+++ home.py
@@ -11,61 +11,56 @@ import signal
 key = 666
 conso= 50
 prod = 100
-
+maisons = 10
 # types : 2 (demande énergie), 3 (don énergie), 4 (vendre au marché), 5 (achat au marché), 6 (fin de communication)
 #remarque il faut utiliser des signaux pour démarrer les jobs
 #utiliser close() et terminate() pour mettre fin à l'utilisation des ressources
 
 def maison(cons,prod,mq):
-    #signal.signal(signal.SIGINT, handler)
-    #print("dans une maison")
-    try :
-        etat = random.randint(1, 3)
-        while True :
-            meteo = random.randint(-10, 30) #ici on ira chercher la valeur dans la mémoire partagée
-            cons = cons - meteo #si temp > 0 alors consommation diminue (l'inverse sinon)
-            if cons<0 :
-                cons = 0
-            print(os.getpid(),": production =",prod,"et consommation =",cons)
-            energie = prod - cons
-            print(os.getpid(),": energie =",energie)
-            if energie<=0: # manque d'nergie
-                message = str(os.getpid())
-                mq.send(message, type=3)
-                print(os.getpid(),": demande énergie")
+    etat = random.randint(1, 3)
+    while True :
+        meteo = random.randint(-10, 30) #ici on ira chercher la valeur dans la mémoire partagée
+        cons = cons - meteo #si temp > 0 alors consommation diminue (l'inverse sinon)
+        if cons<0 :
+            cons = 0
+        print(os.getpid(),": production =",prod,"et consommation =",cons)
+        energie = prod - cons
+        print(os.getpid(),": energie =",energie)
+        if energie<=0: # manque d'nergie
+            message = str(os.getpid())
+            mq.send(message, type=3)
+            print(os.getpid(),": demande énergie")
+            try :
+                don, type = mq.receive(False,2) # on regarde s'il y a des donneurs
+                print(os.getpid(),": reception energie =",int(don))
+                prod  = prod + int(don)
+            except sysv_ipc.BusyError : #si aucune maison ne donne de l'énergie
+                mq.send(msg, type=5)
+                print(os.getpid(),": Aucun donneur, achete l'energie manquante au marché")
+                prod = prod + abs(prod-cons)
+        else :
+            msg = str(energie) # les message dans MessageQueue sont forcément des objets bytes (des string)
+            prod  = prod - energie # on met à jour la production
+            if etat == 1 : # don du surplus
+                mq.send(msg, type=2)
+                print(os.getpid(),": a envoyé son énergie",energie)
+            elif etat == 2 : # vente du surplus
+                mq.send(msg, type=4)
+                print(os.getpid(),": a vendu au marché")
+
+            else : # vente du surplus si aucun mendiant
+                print(os.getpid(),": vérifie s'il y a des mendiant")
                 try :
-                    don, type = mq.receive(False,2) # on regarde s'il y a des donneurs
-                    print(os.getpid(),": reception energie =",int(don))
-                    prod  = prod + int(don)
-                except sysv_ipc.BusyError : #si aucune maison ne donne de l'énergie
-                    mq.send(msg, type=5)
-                    print(os.getpid(),": Aucun donneur, achete l'energie manquante au marché")
-                    prod = prod + abs(prod-cons)
-            else :
-                msg = str(energie) # les message dans MessageQueue sont forcément des objets bytes (des string)
-                prod  = prod - energie # on met à jour la production
-                if etat == 1 : # don du surplus
+                    dem, type = mq.receive(False,3) #on regarde si il y a des demandes mais on ne se bloque pas
                     mq.send(msg, type=2)
-                    print(os.getpid(),": a envoyé son énergie",energie)
-                elif etat == 2 : # vente du surplus
+                    print(os.getpid(),": a donnée",energie,"à maison mendiante")
+                except sysv_ipc.BusyError : #si aucune maison ne demande de l'énergie
                     mq.send(msg, type=4)
-                    print(os.getpid(),": a vendu au marché")
+                    print(os.getpid(),": Aucun mendiant, vend au marché")
 
-                else : # vente du surplus si aucun mendiant
-                    print(os.getpid(),": vérifie s'il y a des mendiant")
-                    try :
-                        dem, type = mq.receive(False,3) #on regarde si il y a des demandes mais on ne se bloque pas
-                        mq.send(msg, type=2)
-                        print(os.getpid(),": a donnée",energie,"à maison mendiante")
-                    except sysv_ipc.BusyError : #si aucune maison ne demande de l'énergie
-                        mq.send(msg, type=4)
-                        print(os.getpid(),": Aucun mendiant, vend au marché")
-
-            print(os.getpid(),": prod =",prod,"et cons=",cons)
-            time.sleep(2)
-            #pas utiliser time.sleep mais mettre un 'tick' dans market pour synchro
-    except KeyboardInterrupt:
-        return
+        print(os.getpid(),": prod =",prod,"et cons=",cons)
+        time.sleep(2)
+        #pas utiliser time.sleep mais mettre un 'tick' dans market pour synchro ou utiliser une barrière avec multiprocessing.Barrier
 
 
 if __name__ == "__main__":
@@ -80,7 +75,7 @@ if __name__ == "__main__":
 
     processes = []
 
-    for i in range(10): #initialise les maisons
+    for i in range(maisons): #initialise les maisons
         process = multiprocessing.Process(target=maison, args=(conso,prod,mq))
         processes.append(process)
         process.start()
--- market.py
+++ market.py
@@ -3,19 +3,30 @@ import sys
 import time
 import sysv_ipc
 import concurrent.futures
+import signal
+import os
+
 
 key = 666
+fin = False
 
 def worker(mq, m, t, cout): #gére  les transactions avec les maisons
-    if t == 4 : # vente
-        print("Maison veut vendre",int(m),"d'energie")
-        cout  = cout - (int(m) * 0.01) # plus d'énergie disponible donc le prix baisse
-    if t == 5 : # achat
-        print("Maison veut acheter",int(m),"d'energie")
-        print("Marché fait la transaction") # es ce que c'est nécessaire d'envoyer quelque chose dans la queue ici ?
-        cout = cout + (int(m) * 0.01) # moins d'énergie disponible donc le prix augmente
-    return cout
+    try :
+        if t == 4 : # vente
+            print("Maison veut vendre",int(m),"d'energie")
+            cout  = cout - (int(m) * 0.01) # plus d'énergie disponible donc le prix baisse
+        if t == 5 : # achat
+            print("Maison veut acheter",int(m),"d'energie")
+            print("Marché fait la transaction") # es ce que c'est nécessaire d'envoyer quelque chose dans la queue ici ?
+            cout = cout + (int(m) * 0.01) # moins d'énergie disponible donc le prix augmente
+        return cout
 
+    except sysv_ipc.SignalError :
+        print("controle C")
+
+def signal_handler(sig, frame):
+    fin = True
+    
 
 if __name__ == "__main__":
     try:
@@ -27,19 +38,25 @@ if __name__ == "__main__":
 
     print("Demarrage MessageQueue.")
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers = 10) as executor: #limite  de 10 pour gérer les transactions avec les maisons
-        cout = 1 # cout de l'energie
-        while True:
-            m, t = mq.receive(True)
-            calc = executor.submit(worker, mq, m, t, cout)
-            cout = calc.result()
-            print("prix actuelle de l'energie est",cout)
-            if cout > 10 :
-                print("Crise économique") # on va signaler economics
-            if t == 6 :
-                mq.remove()
-                ('Fin du processus home')
 
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 10) as executor: #limite  de 10 pour gérer les transactions avec les maisons
+        cout = 5 # cout de l'energie
+        while True:
+                try :
+                    m, t = mq.receive(True)
+                    calc = executor.submit(worker, mq, m, t, cout)
+                    cout = calc.result()
+                    print("prix actuelle de l'energie est",cout)
+                    #if cout > 10 :
+                    #    print("Crise économique") # on va signaler economics
+
+                    signal.signal(signal.SIGINT, signal_handler)
+                
+                except sysv_ipc.Error:
+                    print("\n"+'Fin transactions')
+                    # il faut envoyer un signal au maisons pour leur dire de se terminer 
+                    break
+    
+        print("Fin Market.")
         mq.remove()
 
-    print("Fin Market.")
