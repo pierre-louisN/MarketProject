e12445f515398e82bff6fb02aefeada505cd31b2
xnesmond <72248674+xnesmond@users.noreply.github.com>
2021-01-12 15:11:06 +0100

    Ajout du Pipe
    J'ai juste ajouté le fils, je suppose que c'est toi qui aura le père

--- economic.py
+++ economic.py
@@ -1,30 +1,37 @@
-from multiprocessing import Lock,Process,Value
+from multiprocessing import Lock,Process,Value,Pipe
 from random import *
-from ctypes import c_char
 import multiprocessing
-import signal
-import time
+import sys
+import os
 
 evenement = randint(1,40)
 crash_fincancier = False
 dictature = False
 anarchie = False
 
+#Création du pipe
+parent_conn, child_conn = Pipe()
+
+
 def Event(evenement):
     #Il y a 3 évènements possibles, à chaque synchronisation on lance un randint 
     #Si le randint correspond à l'un des évènement : il devient vrai
-    if evenement == 9 :
-        crash_fincancier = True
-    elif evenement == 13 :
-        dictature = True 
-    elif evenement == 0 : 
-        anarchie = True
-    
+    while True :
+        evenement = randint (1,400)
+        if evenement == 9 :
+            crash_fincancier = True
+            child_conn.send("crash_fincancier")#prévient le père
+        elif evenement == 13 :
+            dictature = True
+            child_conn.send("dictature")#prévient le père
+        elif evenement == 0 : 
+            anarchie = True
+            child_conn.send("anarchie")#prévient le père
 
 if __name__ == "__main__" :
     
     p1 = multiprocessing.Process(target=Event , args = (evenement))
-    p2 = multiprocessing.Process(target=Event , args = (evenement))
+    
     
     p1.start()
     p1.join()
