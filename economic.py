e21e11a32152d8e7757884d2f48d70815c7dde7d
xnesmond <72248674+xnesmond@users.noreply.github.com>
2021-01-12 15:59:54 +0100

    Ajout du Pipe() fin

--- economic.py
+++ economic.py
@@ -13,30 +13,26 @@ anarchie = False
 parent_conn, child_conn = Pipe()
 
 
-def Event(evenement):
+def Event(evenement, child_conn):
     #Il y a 3 évènements possibles, à chaque synchronisation on lance un randint 
     #Si le randint correspond à l'un des évènement : il devient vrai
     while True :
-        evenement = randint (1,400)
+        evenement = randint(1,200)#les évènements ont une chance sur 200 d'arriver
         if evenement == 9 :
             crash_fincancier = True
             child_conn.send("crash_fincancier")#prévient le père
+            print(parent_conn.recv())#c'était pour vérifier si le pipe fonctionnait correctement, j'ai fais un test en fixant evenement à 9
         elif evenement == 13 :
             dictature = True
             child_conn.send("dictature")#prévient le père
-        elif evenement == 0 : 
+        elif evenement == 100 : 
             anarchie = True
             child_conn.send("anarchie")#prévient le père
 
+        
 if __name__ == "__main__" :
     
-    p1 = multiprocessing.Process(target=Event , args = (evenement))
-    
+    p1 = multiprocessing.Process(target=Event , args = (evenement,child_conn))
     
     p1.start()
     p1.join()
-    
-    print(evenement.value)
-    print(crash_fincancier)
-    print(dictature)
-    print(anarchie)
