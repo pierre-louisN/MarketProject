13b13cb06b69a1b9459a5057274ae50d0b3496c6
xnesmond <72248674+xnesmond@users.noreply.github.com>
2021-01-13 13:49:51 +0100

    Petites modifications

--- politics.py
+++ politics.py
@@ -1,36 +1,33 @@
-from multiprocessing import Lock,Process,Value
-from random import *
-from ctypes import c_char
-import multiprocessing
-import signal
-import time
+import sys
+import os
 
-evenement = multiprocessing.Array('i')
-listEvenement = [guerre,dictature,anarchie]
+evenement_P = randint(1,400)
 guerre = False
 dictature = False
 anarchie = False
+listEvenementPolitics = [guerre,dictature,anarchie] #on peut faire une fonction qui lit ce tableau continuellement
 
-def Event(evenement,aleat):
-    evenement.value = aleat
-    if evenement.value == 9 :
-        guerre = True
-    elif evenement.value == 3 :
-        dictature = True 
-    elif evenement.value == 0 : 
-        anarchie = True
+#Création du pipe2
+parent_conn2, child_conn2 = Pipe()
+
+def Event(evenement,child_conn2):
+    while True :
+        evenement = randint(1,400)
+        if evenement == 9 :
+            guerre = True
+            child_conn2.send("guerre")#prévient le père
+        elif evenement == 3 :
+            dictature = True
+            child_conn2.send("dictature")#prévient le père            
+        elif evenement == 0 : 
+            anarchie = True
+            child_conn2.send("anarchie")#prévient le père
     
-
 if __name__ == "__main__" :
-    
-    aleat = randint(1,10)
-    p1 = multiprocessing.Process(target=Event , args = (evenement, aleat))
-    p2 = multiprocessing.Process(target=Event , args = (evenement, aleat))
-    
+        
+    p1 = multiprocessing.Process(target=Event , args = (evenement, child_conn2))
+  
     p1.start()
     p1.join()
     
-    print(evenement.value)
-    print(guerre)
-    print(dictature)
-    print(anarchie)
+   
